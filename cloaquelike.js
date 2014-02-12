9c1eec27c9c6ed69aa7dc3d0cc9e25cb1f5a5b25
Andrew Monks <a@monks.co>
2014-02-12 15:33:21 -0600

    update

--- cloaquelike.js
+++ cloaquelike.js
@@ -3,12 +3,12 @@
 // andrew monks 2014
 
 // function to take an array of image objects and return html
-function cloaquelike(elements) {
+function cloaquelike(elements, container) {
 	for (var i = 0; i <= elements.length - 1; i++) {
 		objectContainer = $('<div class="objectbox ' + elements[i].width + '"></div>');
 		objectContainer.css("height","" + elements[i].height + "px");
 		elements[i].element.appendTo(objectContainer);
-		objectContainer.appendTo(".container");
-		$('<div class="clear"></div>').appendTo(".container")
+		objectContainer.appendTo(container);
+		$('<div class="clear"></div>').appendTo(container)
 	};
 };
