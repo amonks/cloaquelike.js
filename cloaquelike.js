6af969ff1807caba2711bd90c9c9cd4e59976bd6
Andrew Monks <a@monks.co>
2014-02-12 15:27:41 -0600

    init

---
+++ cloaquelike.js
@@ -0,0 +1,14 @@
+// cloaquelike.js
+// vertical scrolling website
+// andrew monks 2014
+
+// function to take an array of image objects and return html
+function cloaquelike(elements) {
+	for (var i = 0; i <= elements.length - 1; i++) {
+		objectContainer = $('<div class="objectbox ' + elements[i].width + '"></div>');
+		objectContainer.css("height","" + elements[i].height + "px");
+		elements[i].element.appendTo(objectContainer);
+		objectContainer.appendTo(".container");
+		$('<div class="clear"></div>').appendTo(".container")
+	};
+};
